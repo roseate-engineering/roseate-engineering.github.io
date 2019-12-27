98ed30cc47ec11cf55beaaeb7b753c6ecb5ea020
Sanchit <sanchitcop19@Gmail.com>
2019-12-27 02:38:54 -0500

    Formatted code and removed pricelists for faster loading

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var app = new Vue(
+    {
+        el: '#menu-item-row',
+        data: {
+            green: "green",
+            menu_items: [
+                {link: "something"},
+                {link: "somehting"}
+            ]
+        }
+    }
+)
