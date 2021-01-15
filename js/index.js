94338c72a8245661af009d34b025ae614bdb12bf
Sanchit <sanchitcop19@Gmail.com>
2021-01-14 21:39:55 -0800

    remove canvas link

--- js/index.js
+++ js/index.js
@@ -6,5 +6,11 @@ const lazyLoad = function(){
     const slideshow = document.getElementById('slideshow');
 }
 
+const centerCanvas = () => {
+    canvases = document.getElementsByClassName('canvasjs-chart-canvas');
+    canvases.forEach(canvas => canvas.style = `${canvas.style};position: relative;`)
+}
+
 document.addEventListener("DOMContentLoaded", lazyLoad);
+document.addEventListener("DOMContentLoaded", centerCanvas);
 
