8ca1789427f6e68d338cb0d4b838057da62e9128
root <root@Shadow-Sanchit.localdomain>
2021-03-19 22:49:24 -0700

    Updated financials

--- js/financials.js
+++ js/financials.js
@@ -4,28 +4,28 @@ drawChart = function (event) {
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         theme: "light2",
-        width: this.window.screen.width*0.8,
-        title:{
+        width: this.window.screen.width * 0.8,
+        title: {
             text: "Sales Turnover (INR)"
         },
-      axisY: {
-          valueFormatString: "##,##,###",
-      },
-        data: [{        
-            type: "line",       
+        axisY: {
+            valueFormatString: "##,##,###",
+        },
+        data: [{
+            type: "line",
             dataPoints: [
                 { label: "2015-16", y: 3500000 },
                 { label: "2016-17", y: 8708000 },
-                { label: "2017-18", y: 10065000},
+                { label: "2017-18", y: 10065000 },
                 { label: "2018-19", y: 17425000 },
                 { label: "2019-20", y: 21580000 },
+                { label: "2020-21", y: 34500000 },
             ]
         }]
     });
     chart.render();
-    
-    }
+
+}
 
 window.addEventListener('load', drawChart)
 window.addEventListener('resize', drawChart)
-    
