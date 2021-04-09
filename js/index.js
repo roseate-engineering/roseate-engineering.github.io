b6d3cacdbf7e9f584c8502c2c8f0199f7bdddabb
Sanchit <sanchitcop19@gmail.com>
2021-04-08 22:23:45 -0700

    Added vendors

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
-const lazyLoad = function(){
+const lazyLoad = function () {
     const lazyImages = document.getElementsByClassName("lazy-img");
-    for (lazyImage of lazyImages){
-        lazyImage.src = lazyImage.dataset.src 
+    for (lazyImage of lazyImages) {
+        lazyImage.src = lazyImage.dataset.src
     }
     const slideshow = document.getElementById('slideshow');
 }
 
-const centerCanvas = () => {
-    canvases = document.getElementsByClassName('canvasjs-chart-canvas');
-    canvases.forEach(canvas => canvas.style = `${canvas.style};position: relative;`)
+const sheetsApiKey = 'AIzaSyCF86I8sGsInl2gmAypZxg96nswVIXkeHQ'
+const loadCustomers = async () => {
+    await gapi.client.init({
+        'apiKey': sheetsApiKey,
+    });
+    const resp = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1Ugs9D6cSm5QdcUQhji0p_F5Ug86dSAmzx2eIaE4Rzic',
+    });
+    console.log(resp);
+
 }
 
 document.addEventListener("DOMContentLoaded", lazyLoad);
-document.addEventListener("DOMContentLoaded", centerCanvas);
+document.addEventListener("DOMContentLoaded", loadCustomers);
 
